be9e5077b8636ae18853c0c2f5bf36ebb4446ebe
CityMado <stenius.jere@gmail.com>
2020-02-07 11:08:59 +0200

    Can't remember xd

---
+++ Kiinarymi/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform Cannon;
+    public GameObject BulletPrefab;
+    public float fireRate = 0.5f;
+    public float nextFire = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Shoot();
+        }
+    }
+    private void Shoot()
+    {
+        var bullet = Instantiate(BulletPrefab);
+        bullet.transform.rotation = transform.rotation;
+        bullet.transform.position = transform.position + transform.up;
+        Debug.Log("Fired");
+    }
+}
