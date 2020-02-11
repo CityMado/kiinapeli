c000f1943578379d7b2a5e4d1b5498ac2d77b4df
CityMado <stenius.jere@gmail.com>
2020-02-11 10:32:21 +0200

    added rotator

---
+++ Kiinarymi/Assets/Scripts/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public int x;
+    public int z;
+    public int y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(Time.deltaTime*x,y,z));
+    }
+}
