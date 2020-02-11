abbf25823847022e561f3e6600220b3aef4a1dee
CityMado <stenius.jere@gmail.com>
2020-02-11 11:51:31 +0200

    Perseelleen vituiks

--- Kiinarymi/Assets/Scripts/PlayerController.cs
+++ Kiinarymi/Assets/Scripts/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0) && Time.time > nextFire)
+        if (Input.GetMouseButtonDown(0) && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
             Shoot();
